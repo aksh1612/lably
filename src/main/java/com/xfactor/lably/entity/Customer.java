849ad482f3c2e3e8e6a5b884c96d988c3725e814
Aksh-1612 <ankitagarwal66201@gmail.com>
2021-05-16 22:23:39 +0530

    entity

---
+++ src/main/java/com/xfactor/lably/entity/Customer.java
@@ -0,0 +1,15 @@
+package com.xfactor.lably.entity;
+
+public class Lab {
+
+    private String name;
+
+    private String address;
+
+    private String phone;
+
+    private String gender;
+
+    
+
+}
